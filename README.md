8a58a4c6de5fec6e08c9165f0e45a2df3e8829b7
spirolad <spiroles@laposte.net>
2022-05-15 17:44:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blog-NodeJs
+# Blog-NodeJs
---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const blogRoutes = require('./routes/blogsRoutes');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const dbURI = "mongodb+srv://rooot:spirolad@node.pha9j.mongodb.net/Node?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'About'});
+});
+
+
+app.use('/blogs', blogRoutes);
+
+app.use((req, res) => {
+    res.status(404).render('404', {title: '404 Not Found'});
+});
